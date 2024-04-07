792a44ea229ff60b675355fd8d5be72df1c75752
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-04-07 16:14:15 -0300

    att

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request
+from repository.database import db
+from models.payment import Payment
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "SECRET-KEY-WEBSOCKET"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:senha123@localhost/payments'
+
+db.init_app(app)
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message": "Invalid value"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], 
+                          expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({"message": "The payment has been created",
+                    "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify({"message": "The payment has been confirmed"})
+
+@app.route('/payments/pix/<int:paymeny_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'Pagamento pix'
+
+if __name__ == '__main__':
+    app.run(debug=True)
